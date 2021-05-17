e5a71456764aa7f79905ee0ac06a6580f985de3d
Fernando Frazão da Silva <fernando.frazao@emater.df.gov.br>
2021-05-17 07:49:28 -0300

    adiçao do campo observaçao
    Signed-off-by: Fernando Frazão da Silva <fernando.frazao@emater.df.gov.br>

--- src/main/java/com/frazao/adubacaodescomplicada/modelo/entidade/adubacaodescomplicada/AnaliseSoloParametro.java
+++ src/main/java/com/frazao/adubacaodescomplicada/modelo/entidade/adubacaodescomplicada/AnaliseSoloParametro.java
@@ -8,6 +8,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
@@ -39,20 +40,24 @@ public class AnaliseSoloParametro extends EntidadeBaseTemId<Integer> {
 	@Column(name = "nome")
 	private String nome;
 
+	@Column(name = "observacao")
+	@Lob
+	private String observacao;
+
 	@Column(name = "ordem")
 	private Integer ordem;
-	
+
 	@Column(name = "sigla")
 	private String sigla;
-	
+
 	@Column(name = "tem_formula_qualidade_solo")
 	@Enumerated(EnumType.STRING)
 	private Confirmacao temFormulaQualidadeSolo;
-	
+
 	@JoinColumn(name = "unidade_medida_id")
 	@ManyToOne
 	private UnidadeMedida unidadeMedida;
-	
+
 	public AnaliseSoloParametro(Integer id) {
 		super(id);
 	}
